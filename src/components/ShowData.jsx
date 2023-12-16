d63b3f50d3426de11def058723a36597355a28fb
Utkarsh Gupta <utkarshgupta04092003@gmail.com>
2023-12-16 11:29:27 +0530

    useeffect in showdata is not working for dynamic data after change from parent

--- src/components/ShowData.jsx
+++ src/components/ShowData.jsx
@@ -4,7 +4,34 @@ import React, { useEffect, useState } from 'react';
 const ShowData = ({ data, isLoading }) => {
     const [typedData, setTypedData] = useState('');
     // const data = 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quod tenetur laudantium soluta ratione officiis eos. Quos possimus voluptatum commodi officia aut, culpa blanditiis tempora. Assumenda reprehenderit ipsam dignissimos repudiandae laborum.';
-   
+    useEffect(() => {
+        let i = 0;
+        let interval;
+        setTypedData('');
+        const typing = () => {
+            interval = setInterval(() => {
+                setTypedData((prevTypedData) => {
+                    const currentChar = data[i];
+                    i++;
+
+                    return prevTypedData + (currentChar ? currentChar : ''); // Check for undefined
+                });
+
+                if (i > data.length) {
+                    clearInterval(interval);
+                }
+            }, 50);
+        };
+
+        clearInterval(interval);
+        typing();
+
+        return () => {
+            // Cleanup function to clear the interval when the component is unmounted
+            clearInterval(interval);
+        };
+    }, [data]);
+
     return (
         <div className='mt-3 text-justify'>
             <p className='font-bold'>Generated Sentences : </p>
